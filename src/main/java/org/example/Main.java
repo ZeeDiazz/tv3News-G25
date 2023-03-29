8bbc2b235fd87cb7f2ecbc4ffb4854d359a304da
ZEE <Zaidsh02@gmail.com>
2023-03-30 01:24:42 +0200

    Insert into working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,34 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String host = "localhost"; //host is "localhost" or "127.0.0.1"
-        String port = "3306"; //port is where to communicate with the RDBMS
-        String database = "tv3NewsDB"; //database containing tables to be queried
-        String cp = "utf8"; //Database codepage supporting Danish (i.e. æøåÆØÅ)
+        String host = "localhost";
+        String port = "3306";
+        String database = "tv3NewsDB";
+        String cp = "utf8";
         // Set username and password.
-        String username = "root";		// Username for connection
-        String password = "Mypassword01";	// Password for username
+        String username = "root";
+        String password = "Mypassword01";
+
         String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?characterEncoding=" + cp;
         try {
-            Scanner scanner = new Scanner(System.in, "CP850"); //Western Europe Console CodePage
+            Scanner scan = new Scanner(System.in, "CP850");
+
             // Get a connection.
             Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+
+            System.out.println("Insert values in tables:");
+            String SQLInsertion = scan.nextLine();
+            statement.execute(SQLInsertion);
+
+            statement.close();
+            connection.close();
         }catch (Exception e) {
             e.printStackTrace();
         }
