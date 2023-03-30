a0ceadb03f99e6c64a55b71d73f3b9204b718bcd
Zigalow <s224302@dtu.dk>
2023-03-30 17:44:16 +0200

    Latest update to Tv3Database

--- src/main/java/org/example/Tv3Database.java
+++ src/main/java/org/example/Tv3Database.java
@@ -1,49 +1,98 @@
 package org.example;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+
 
 public class Tv3Database implements Database {
 
 
+    Connection connection;
+    Statement statement;
+
     @Override
     public void login(String host, String port, String username, String password, String databaseName) throws SQLException {
-
+        login(host, port, username, password, databaseName, "utf8");
     }
 
     @Override
     public void login(String host, String port, String username, String password, String databaseName, String characterEncoding) throws SQLException {
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?characterEncoding=" + characterEncoding;
 
+        // Get a connection.
+        this.connection = DriverManager.getConnection(url, username, password);
+        this.statement = connection.createStatement();
     }
 
     @Override
     public ResultSet executeQuery(String query) throws SQLException {
-        return null;
+        return this.statement.executeQuery(query);
+
     }
 
     @Override
     public ResultSet executeSanitisedQuery(String query) throws SQLException {
-        return null;
+        return this.executeQuery(query);
     }
 
     @Override
     public boolean reporterExists(Reporter reporter) {
-        return false;
+        Reporter databaseReporter = getReporter(reporter.getCPR() + "");
+        if (databaseReporter == null) {
+            return false;
+        }
+        return databaseReporter.equals(reporter);
     }
 
     @Override
     public Reporter getReporter(String cpr) {
+        String basicQuery = "Select cpr FROM Journalist WHERE cpr == " + cpr + ";";
+        try {
+            ResultSet resultSet = this.executeQuery(basicQuery);
+
+            resultSet.beforeFirst();
+            if (!resultSet.next()) {
+                return null;
+            }
+
+            // Get the info of the reporter
+            String firstName = resultSet.getString("firstName");
+            String lastName = resultSet.getString("lastName");
+            String streetName = resultSet.getString("streetName");
+            int civicNumber = resultSet.getInt("civicNumber");
+            int zipCode = resultSet.getInt("zipCode");
+            String country = resultSet.getString("country");
+
+            return new Reporter(Integer.parseInt(cpr), firstName, lastName, streetName, civicNumber, zipCode, country);
+        } catch (SQLException e) {
+            // ignored
+        }
         return null;
     }
 
     @Override
     public void insertReporter(Reporter reporter) throws EntryExistsException {
-
+        if (reporterExists(reporter)) {
+            throw new EntryExistsException();
+        }
+        // TODO insert
     }
 
     @Override
     public boolean updateReporter(Reporter reporter) {
-        return false;
+        if (reporterExists(reporter)) {
+            // TODO update data
+            return true;
+        }
+        else {
+            try {
+                insertReporter(reporter);
+                return true;
+            }
+            catch (EntryExistsException e) {
+                // Should never happen (already checked)
+                return false;
+            }
+        }
     }
 
     @Override
