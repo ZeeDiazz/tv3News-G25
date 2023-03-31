eb15f1167731ce161530fdde6deda1f4f9edc262
Daniel Weper Jensen <danielweper@gmail.com>
2023-03-31 09:36:53 +0200

    Main done (hopefully)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import javax.xml.crypto.Data;
 import java.io.*;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -31,89 +32,151 @@ public class Main {
             chosen = scan.next();
             scan.nextLine();
 
-            if (chosen.equals("1")) {
-                // TODO make sure it handles specifically footage and reporters
-                while (true) {
-                    System.out.println("What do you want to insert?");
-                    System.out.println("1. Reporter");
-                    System.out.println("2. Footage");
+            switch (chosen) {
+                case "1":
+                    boolean inserting = true;
+                    while (inserting) {
+                        System.out.println("What do you want to insert?");
+                        System.out.println("0. Go back");
+                        System.out.println("1. Reporter");
+                        System.out.println("2. Footage");
 
-                    chosen = scan.next();
-                    if (chosen.equals("1")) {
-                        // TODO
+                        chosen = scan.next();
+                        switch (chosen) {
+                            case "0":
+                                inserting = false;
+                                break;
+                            case "1":
+                                System.out.println("Please enter the CPR number of the new reporter:");
+                                String cprString = scan.nextLine();
 
-                        try {
-                            database.insertReporter(null);
-                        } catch (EntryExistsException e) {
-                            System.out.println("The CPR number you entered already exists in the database");
+                                if (database.reporterExists(cprString)) {
+                                    System.out.println("The CPR number you entered already exists in the database");
+                                    System.out.println("Do you want to overwrite this data? (Y/n)");
+
+                                    chosen = scan.next();
+                                    if (chosen.equalsIgnoreCase("n")) {
+                                        continue;
+                                    }
+                                }
+
+                                System.out.println("Enter the firstname of the reporter:");
+                                String firstName = scan.nextLine();
+                                System.out.println("Enter the lastname of the reporter:");
+                                String lastName = scan.nextLine();
+                                System.out.println("Enter the street name of the reporter:");
+                                String streetName = scan.nextLine();
+                                System.out.println("Enter the civic number of the reporter");
+                                int civicNumber = scan.nextInt();
+                                System.out.println("Enter the zip code of the reporter");
+                                int zipCode = scan.nextInt();
+                                System.out.println("Enter the country of the reporter");
+                                String country = scan.nextLine();
+                                Reporter reporter = new Reporter(Integer.parseInt(cprString), firstName, lastName, streetName, civicNumber, zipCode, country);
+
+                                if (database.updateReporter(reporter)) {
+                                    System.out.println("Updated succesfully");
+                                }
+                                else {
+                                    System.out.println("Something went wrong");
+                                }
+                                break;
+                            case "2":
+                                System.out.println("Please enter the title of the new footage:");
+                                String footageTitle = scan.nextLine();
+
+                                if (database.footageExists(footageTitle)) {
+                                    System.out.println("The title you entered already exists in the database");
+                                    System.out.println("Do you want to overwrite this data? (Y/n)");
+
+                                    chosen = scan.next();
+                                    if (chosen.equalsIgnoreCase("n")) {
+                                        continue;
+                                    }
+                                }
+
+                                System.out.println("Enter the date (yyyyMMdd) of the footage:");
+                                String dateString = scan.nextLine();
+                                LocalDate shootingDate = LocalDate.of(
+                                        Integer.parseInt(dateString.substring(0, 4)),
+                                        Integer.parseInt(dateString.substring(4, 6)),
+                                        Integer.parseInt(dateString.substring(6))
+                                );
+                                System.out.println("Enter the duration of the footage:");
+                                int duration = scan.nextInt();
+                                Footage footage = new Footage(footageTitle, shootingDate, duration);
+
+                                if (database.updateFootage(footage)) {
+                                    System.out.println("Updated succesfully");
+                                }
+                                else {
+                                    System.out.println("Something went wrong");
+                                }
+                                break;
+                            default:
+                                System.out.println("Please choose one of the listed options");
+                                break;
                         }
-                        break;
-                    } else if (chosen.equals("2")) {
-                        // TODO
-                        break;
-                    } else {
-                        System.out.println("Please choose one of the listed options");
-                        continue;
                     }
-                }
-            } else if (chosen.equals("2")) {
-                System.out.println("Type SQL Query:");
-                System.out.println("");
-                String query = scan.nextLine();
-                ResultSet resultSet;
-                try {
-                    resultSet = database.executeSanitisedQuery(query);
+                case "2":
+                    System.out.println("Type SQL Query:");
+                    System.out.println("");
+                    String query = scan.nextLine();
+                    ResultSet resultSet;
+                    try {
+                        resultSet = database.executeSanitisedQuery(query);
 
-                    ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-                    int columnCount = resultSetMetaData.getColumnCount();
+                        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+                        int columnCount = resultSetMetaData.getColumnCount();
 
-                    // Print all attribute names.
-                    // --------------------------
-                    for (int i = 1; i <= columnCount; i++) {
-                        System.out.print(resultSetMetaData.getColumnName(i) + "; ");
-                    }
-                    System.out.println();
-                    System.out.println("------");
-
-                    // Print all table rows.
-                    // ---------------------
-                    resultSet.beforeFirst(); // Set pointer for resultSet.next()
-                    while (resultSet.next()) {
-                        // Print all values in a row.
+                        // Print all attribute names.
                         // --------------------------
                         for (int i = 1; i <= columnCount; i++) {
-                            if (resultSet.getString(i) == null) {
-                                System.out.print("null; ");
-                            } else {
-                                System.out.print(resultSet.getString(i) + "; ");
-                            }
+                            System.out.print(resultSetMetaData.getColumnName(i) + "; ");
                         }
                         System.out.println();
+                        System.out.println("------");
+
+                        // Print all table rows.
+                        // ---------------------
+                        resultSet.beforeFirst(); // Set pointer for resultSet.next()
+                        while (resultSet.next()) {
+                            // Print all values in a row.
+                            // --------------------------
+                            for (int i = 1; i <= columnCount; i++) {
+                                if (resultSet.getString(i) == null) {
+                                    System.out.print("null; ");
+                                } else {
+                                    System.out.print(resultSet.getString(i) + "; ");
+                                }
+                            }
+                            System.out.println();
+                        }
+                    } catch (SQLException e) {
+                        System.out.println("Something went wrong when executing your query");
                     }
-                } catch (SQLException e) {
-                    System.out.println("Something went wrong when executing your query");
-                    continue;
-                }
-            } else if (chosen.equals("3")) {
-                System.out.println("Enter filename:");
-                String filename = scan.nextLine();
-                if (!filename.endsWith(".csv")) {
-                    filename += ".csv";
-                }
-                filename = "src/main/resources/" + filename;
-                FootagesAndReportersLoader loader = new FootagesAndReportersLoader();
-                try {
-                    List<FootageAndReporter> footagesAndReporters = loader.loadFootagesAndReporters(filename);
-                    for (FootageAndReporter footageAndReporter : footagesAndReporters) {
-                        database.updateReporter(footageAndReporter.getReporter());
-                        database.updateFootage(footageAndReporter.getFootage());
+                    break;
+                case "3":
+                    System.out.println("Enter filename:");
+                    String filename = scan.nextLine();
+                    if (!filename.endsWith(".csv")) {
+                        filename += ".csv";
                     }
-                } catch (IOException e) {
-                    // ignored
-                }
-            } else {
-                System.out.println("YEE");
-                break;
+                    filename = "src/main/resources/" + filename;
+                    FootagesAndReportersLoader loader = new FootagesAndReportersLoader();
+                    try {
+                        List<FootageAndReporter> footagesAndReporters = loader.loadFootagesAndReporters(filename);
+                        for (FootageAndReporter footageAndReporter : footagesAndReporters) {
+                            database.updateReporter(footageAndReporter.getReporter());
+                            database.updateFootage(footageAndReporter.getFootage());
+                        }
+                    } catch (IOException e) {
+                        // ignored
+                    }
+                    break;
+                default:
+                    System.out.println("YEE");
+                    break;
             }
         }
     }
